1b5e78b8b2a78ce3b5b685f394f182a08b758686
Raul Madero <r.madero.ramirez@gmail.com>
2023-06-12 11:43:36 -0600

    Se rellenó el rectangulo del canvas

---
+++ main.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+
+window.addEventListener('load', startGame);
+function startGame() {
+    //Como vamos a llenar el rectangulo 
+    game.fillRect(0,0,100,100)
+    //Para borrar el rectangulo
+    game.clearRect(0,0,50,50)
+}
