631c406555b2152d43cbae29a0cf88711b93e6ad
Raul Madero <r.madero.ramirez@gmail.com>
2023-06-12 14:01:47 -0600

    Se agregaron funciones al canvas

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', startGame);
 function startGame() {
     //Como vamos a llenar el rectangulo 
-    game.fillRect(0,0,100,100)
+    // game.fillRect(0,0,100,100)
     //Para borrar el rectangulo
-    game.clearRect(0,0,50,50)
+    // game.clearRect(0,0,50,50)
+    // Agregar Texto
+    game.font = '25px Verdana'
+    game.fillStyle = 'purple'
+    game.textAlign = 'center'
+    game.fillText('Rulax', 100, 100)
 }
