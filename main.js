ffb107ee7213d7ecfe5ad40d448549138be13773
Raul Madero <r.madero.ramirez@gmail.com>
2023-06-20 15:09:34 -0600

    Se añadio al jugador

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up')
+const btnLeft = document.querySelector('#left')
+const btRight = document.querySelector('#right')
+const btnDown = document.querySelector('#down')
+const playerPosition = {
+    x: undefined,
+    y: undefined
+}
 let canvaSize;
 let elementSize
 
-
+btnUp.addEventListener('click', moveUp)
+btnLeft.addEventListener('click', moveLeft)
+btRight.addEventListener('click', moveRight)
+btnDown.addEventListener('click', moveDown)
+window.addEventListener('keydown', moveKeys)
 window.addEventListener('load', setCanvaSize);
 window.addEventListener('resize', setCanvaSize)
 // function startGame() {
@@ -27,12 +39,16 @@ function fillMap() {
     const map = maps[0]
     const mapRows = map.trim().split('\n')
     const mapCols = mapRows.map(row => row.trim().split(''))
-    console.log(map, mapRows, mapCols);
     mapCols.forEach((row, rowI) => {
         row.forEach((col, colI) => {
             const emoji = emojis[col]
             const posX = elementSize * (colI + 1)
             const posY = elementSize * (rowI + 1)
+            if(col == 'O') {
+                playerPosition.x = posX
+                playerPosition.y = posY
+            }
+            game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y)
             game.fillText(emoji, posX, posY)
         })
     });
@@ -51,3 +67,21 @@ function fillMap() {
     // game.fillStyle = 'purple'
     // game.textAlign = 'center'
     // game.fillText('Rulax', 100, 100)
+function moveUp(event) {
+    playerPosition.y - 5px
+}
+function moveLeft() {
+    console.log('Me quiero mover hacia izquierda')
+}
+function moveRight() {
+    console.log('Me quiero mover hacia derecha')
+}
+function moveDown() {
+    console.log('Me quiero mover hacia abajo')
+}
+function moveKeys(event){
+    if(event.key == "ArrowUp") moveUp()
+    else if(event.key == "ArrowLeft")  moveLeft()
+    else if(event.key == "ArrowRight")  moveRight()
+    else if(event.key == "ArrowDown") moveDown()
+}
